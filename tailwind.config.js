59ef13bb5a1ee8f9d0de47a4af1444da170332cc
Parulpathakkk <lifesick1998@gmail.com>
2023-10-03 15:06:37 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ["Space Mono, monospace"],
+        bebas: ["Bebas Neue,  sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
